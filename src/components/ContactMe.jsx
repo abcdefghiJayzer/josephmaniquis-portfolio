33951cd1e00f03985a474158ce5616ce8f42c991
Joseph Maniquis <josephmaniquis0@gmail.com>
2025-02-01 13:30:57 +0800

    email function and auto reply

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,41 +1,24 @@
 import React from "react";
-import './../css/ContactMe.css'
+import './../css/ContactMe.css';
+import ContactForm from '../javascript/ContactForm';
 
-class ContactMe extends React.Component { // Extend from React.Component
+class ContactMe extends React.Component {
   render() {
     return (
-      <div className=" pt-10 bg-neutral-900 mx-auto">
-          <div className="contact">
-          <div className="text-ne">
-              <i class="fa-brands fa-github" ></i>
-              <i class="fa-brands fa-facebook" ></i>
-              <i class="fa-brands fa-x-twitter" ></i>
-              <i class="fa-brands fa-linkedin"></i>
-            </div>
-            <div className="md:w-1/2 mx-auto">
-              <h2>Let's work together!</h2>
-              <form action="#" method="post" id="contact_form" onSubmit={(e) => e.preventDefault()}>
-                <div className="name">
-                  <label htmlFor="name"></label>
-                  <input type="text" placeholder="Name" name="name" id="name_input" required />
-                </div>
-                <div className="email">
-                  <label htmlFor="email"></label>
-                  <input type="email" placeholder="E-mail"  name="email" id="email_input" required />
-                </div>
-                <div className="subject">
-                  <label htmlFor="subject"></label>
-                </div>
-                <div className="message">
-                  <label htmlFor="message"></label>
-                  <textarea name="message" placeholder="Message" id="message_input" cols="30" rows="5" required></textarea>
-                </div>
-                <div className="submit">
-                  <input type="submit" value="Submit" id="form_button" />
-                </div>
-              </form>
-            </div>
+      <div className="pt-10 bg-neutral-900 mx-auto">
+        <div className="contact">
+          {/* <div className="social-icons text-ne">
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-x-twitter"></i>
+            <i className="fa-brands fa-linkedin"></i>
+          </div> */}
+          <div className="md:w-1/2 text-white mx-auto">
+            <h2>Let's work together!</h2>
+            {}
+            <ContactForm />
           </div>
+        </div>
       </div>
     );
   }
