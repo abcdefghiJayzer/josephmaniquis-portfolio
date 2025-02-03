609eb11d685b086b1fcd110c04ef355506bacefb
Joseph Maniquis <josephmaniquis0@gmail.com>
2025-02-03 09:26:39 +0800

    contact me resume section updated

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -20,41 +20,53 @@ class ContactMe extends React.Component {
           {/* Social Icons and Contact Form Section */}
           <div className="flex flex-col flex-1 lg:flex-row justify-between items-center md:items-start space-y-6 md:space-y-0">
             {/* Social Media Icons */}
-            <div className="flex flex-1 justify-left space-x-12 md:w-1/4">
-              <a
-                href="https://www.linkedin.com/in/joseph-maniquis/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-neutral-400 hover:text-blue-500 transition-colors duration-300"
-              >
-                <i className="fa-brands fa-linkedin fa-2x"></i>
-              </a>
-              <a
-                href="https://github.com/abcdefghiJayzer"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-neutral-400 hover:text-white transition-colors duration-300"
-              >
-                <i className="fa-brands fa-github fa-2x"></i>
-              </a>
-              <a
-                href="https://www.instagram.com/abcdefghijayzer"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-neutral-400 hover:text-pink-800 transition-colors duration-300"
-              >
-                <i className="fa-brands fa-instagram fa-2x"></i>
-              </a>
+            <div className="">
+              <div className="flex flex-1 justify-left items-center md:w-1/4">
+                <a
+                  href="https://www.linkedin.com/in/joseph-maniquis/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-neutral-400 hover:text-blue-500 transition-colors duration-300"
+                >
+                  <i className="fa-brands fa-linkedin fa-2x pr-12"></i>
+                </a>
+                <a
+                  href="https://github.com/abcdefghiJayzer"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-neutral-400 hover:text-white transition-colors duration-300"
+                >
+                  <i className="fa-brands fa-github fa-2x pr-12"></i>
+                </a>
+                <a
+                  href="https://www.instagram.com/abcdefghijayzer"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-neutral-400 hover:text-pink-800 transition-colors duration-300"
+                >
+                  <i className="fa-brands fa-instagram fa-2x pr-12"></i>
+                </a>
 
-              <a
-                href="/Jayzer-Joseph-Maniquis-Resume.pdf"
-                download
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-neutral-400 hover:text-orange-400 transition-colors duration-300"
-              >
-                <i class="fa-solid fa-file-arrow-down fa-2x"></i>
-              </a>
+                <a
+                  href="/Jayzer-Joseph-Maniquis-Resume.pdf"
+                  download
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-neutral-400 hover:text-orange-400 transition-colors duration-300"
+                >
+                  <i class="fa-solid fa-file-arrow-down fa-2x pr-6"></i>
+                </a>
+
+                <p className="text-xs italic font-bold">Download Resume</p>
+              </div>
+
+              <div className="flex flex-col items-center space-x-10 pt-10">
+                <div className="text-[15px] font-light text-left w-full italic ">
+                  P.S. Hire me or frame my resume—either way, it’s a great
+                  download!{" "}
+                </div>
+                
+              </div>
             </div>
 
             {/* Contact Form Section */}
